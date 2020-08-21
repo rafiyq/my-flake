127c11fe3ed20960bf8b0907e3ce779bc9823bd3
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-08-21 13:53:05 +0700

    Add stable, unstable, home-manager, wayland modules

--- flake.nix
+++ flake.nix
@@ -1,7 +1,17 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+  description = "NixOS reproducible environment with flakes";
+  inputs = {
+    stable.url = "github:NixOS/nixpkgs/nixos-20.03";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home.url = "github:rycee/home-manager/bqv-flakes";
+    home.inputs.nixpkgs.follows = "stable";
 
-  outputs = { self, nixpkgs }: {
+    wayland.url = "github:colemickens/nixpkgs-wayland";
+    wayland.inputs.nixpkgs.follows = "cmpkgs";
+    wayland.inputs.master.follows = "master";
+
+  outputs = { self, stable, unstable, home, wayland }: {
 
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
