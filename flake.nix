c55a9bafca079d450a7a0ae55dd9527e6155b1d4
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-08-28 11:07:31 +0700

    Cleanup file

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
-<<<<<<< HEAD
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-=======
-    stable.url = "github:NixOS/nixpkgs/nixos-20.03";
->>>>>>> master
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home = {
       url = "github:rycee/home-manager/bqv-flakes";
@@ -12,12 +8,7 @@
     };
   };
 
-<<<<<<< HEAD
   outputs = { self, nixpkgs, unstable, home }: {
-=======
-  outputs = { self, stable, unstable, home }: {
->>>>>>> master
-
     nixosConfigurations.container = stable.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
