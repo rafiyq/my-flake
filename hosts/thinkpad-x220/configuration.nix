7ed5364f0c7eece8995785fa1aa82fa2a20346df
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-06 03:27:17 +0000

    Update host thinkpad-x220

--- hosts/thinkpad-x220/configuration.nix
+++ hosts/thinkpad-x220/configuration.nix
@@ -2,100 +2,105 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, inputs, ... }: {
+{ config, pkgs, ... }:
 
-  # boot
-  boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  
-  # flake support
-  nixpkgs.config.allowUnfree = true;
-  nix = {
-    allowedUsers = [ "@wheel" ];
-    trustedUsers = [ "root" "@wheel" ];
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-  
-  # to enable brightness keys 'keys' value may need updating per device
-  programs.light.enable = true;
-  services.actkbd = {
-    enable = true;
-    bindings = [
-      {
-        keys = [ 225 ];
-        events = [ "key" ];
-        command = "/run/current-system/sw/bin/light -A 5";
-      }
-      {
-        keys = [ 224 ];
-        events = [ "key" ];
-        command = "/run/current-system/sw/bin/light -U 5";
-      }
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
     ];
-  };
-  
-  # power management features
-  services.tlp.enable = true;
-  services.tlp.extraConfig = ''
-    CPU_SCALING_GOVERNOR_ON_AC=performance
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    CPU_HWP_ON_AC=performance
-  '';
-  services.logind.lidSwitch = "suspend";
-  
-  hardware.bluetooth.enable = true;
 
-  hardware = {
-    bluetooth.enable = true;
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-  };
+  # Use the GRUB 2 boot loader.
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  i18n.defaultLocale = "en_GB.UTF-8";
-  console.keyMap = "gb";
-  time.timeZone = "Asia/Jakarta";
-  
-  networking = {
-    hostName = "thinkpad-x220";
-    networkmanager.enable = true;
-  };
-  
-  hardware.pulseaudio = {
-    enable = true;
-    tcp.enable = true;
-    support32Bit = true;
-    package = pkgs.pulseaudioFull;
-  };
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  fonts = {
-    enableFontDir = true;
-    enableGhostscriptFonts = true;
-    fontconfig.dpi = 192;
-    fontconfig.defaultFonts.monospace = [ "Iosevka" ];
-    enableDefaultFonts = true;
-    fonts = with pkgs; [ ];
-  };
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s25.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
 
-  users.defaultUserShell = pkgs.zsh;
-  environment.systemPackages = with pkgs; [ ];
-  users.extraUsers.rafiyq = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = [ 
-      "users" "wheel" "networkmanager" "kvm" "libvirtd"
-      "docker" "transmission" "audio" "video" "sway"
-      "sound" "pulse" "input" "render" "dialout"
-    ];
-  };
-  
-  #services.mingetty.autologinUser = "rafiyq";
-  #environment.loginShellInit = ''
-  #  [[ "$(tty)" == /dev/tty? ]] && sudo /run/current-system/sw/bin/lock this
-  #  [[ "$(tty)" == /dev/tty1 ]] && sway
-  #'';
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  system.stateVersion = "20.03";
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
 }
+
--- hosts/thinkpad-x220/default.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, config, lib, pkgs, ... }:
-
-{
-  imports = [ 
-    ./core/default.nix
-    ./misc/default.nix
-  ];
-}
