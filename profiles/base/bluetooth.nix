b58ac7a5f6ed671c280b6fba7001dc739ab4bd13
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-06 04:55:55 +0000

    disable bluetooth audio temporarely

--- profiles/base/bluetooth.nix
+++ profiles/base/bluetooth.nix
@@ -17,13 +17,13 @@
     };
   };
   
-  hardware.pulseaudio = {
-    package = pkgs.pulseaudio.override { bluetoothSupport = true; };
-    extraConfig = ''
-      load-module module-switch-on-connect
-      load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"
-      load-module module-bluetooth-policy
-    '';
-    extraModules = with pkgs; [ pulseaudio-modules-bt ];
-  };
+  #hardware.pulseaudio = {
+  #  package = pkgs.pulseaudio.override { bluetoothSupport = true; };
+  #  extraConfig = ''
+  #    load-module module-switch-on-connect
+  #    load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"
+  #    load-module module-bluetooth-policy
+  #  '';
+  #  extraModules = with pkgs; [ pulseaudio-modules-bt ];
+  #};
 }
