8e08e6bd113dc297fa2b5939afb9d97ee16f9c3f
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-11 06:03:42 +0700

    Add internationalization

--- profiles/base/console.nix
+++ profiles/base/console.nix
@@ -3,9 +3,11 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, lib, pkgs, inputs, ... }: {
-  
+ 
+  i18n.defaultLocale = "en_US.UTF-8";
   console = {
     earlySetup = true;
+    font = "Lat2-Terminus16";
     keyMap = "uk";
   };
   time.timeZone = "Asia/Jakarta";
