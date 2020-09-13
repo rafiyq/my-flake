86d7a53b152b3c6c376d787296d616882ef0120c
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-13 23:48:10 +0700

    Integrated emacs to core module

--- modules/core.nix
+++ modules/core.nix
@@ -4,21 +4,11 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.rafiyq = {
-    home.stateVersion = "20.09";
-    home.keyboard.layout = "gb";
-    home.homeDirectory = "/home/rafiyq";
-    home.packages = with pkgs; [
-      #cli
-      wget curl htop
-      unrar unzip zip
-      sshfs cifs-utils ms-sys ntfs3g
-      gptfdisk parted
-      aria2 youtube-dl
-
-      #other
-      vanilla-dmz
-      #yaru-theme
-    ];
+        # Emacs
+    programs.emacs = {
+      enable = true;
+      #package = pkgs.emacsGit;
+    };
 
     # Git
     programs.git = {
@@ -88,7 +78,21 @@
 	      egrep = "egrep --color=auto";
 	    };
     };
-  
+
+    home.homeDirectory = "/home/rafiyq";
+    home.packages = with pkgs; [
+      #cli
+      wget curl htop
+      unrar unzip zip
+      sshfs cifs-utils ms-sys ntfs3g
+      gptfdisk parted
+      aria2 youtube-dl
+
+      #other
+      vanilla-dmz
+      #yaru-theme
+    ];
+
     systemd.user.startServices = true;
   };
 }
--- modules/programs/emacs.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }: {
-
-  home-manager.users.rafiyq = {
-    programs.emacs = {
-      enable = true;
-      package = pkgs.emacsGit;
-    };
-  };
-}
