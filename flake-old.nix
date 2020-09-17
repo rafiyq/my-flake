e9a83aef5a71b96b028ad1bedce1bb7c1d0e449f
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-17 15:01:12 +0000

    Backup old flake.nix file

---
+++ flake-old.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home.url = "github:rycee/home-manager/bqv-flakes";
+    emacs.url = "github:nix-community/emacs-overlay";
+    hardware.url = "github:NixOS/nixos-hardware";
+  };
+
+  outputs = { self, nix, ... }@inputs: {
+    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+       { 
+         nixpkgs.overlays = [ inputs.emacs.overlay ];
+         system.configurationRevision = 
+           if self ? rev
+           then self.rev
+           else throw "Refusing to build from a dirty Git tree!";
+       }
+
+       inputs.hardware.nixosModules.lenovo-thinkpad-x220
+       (import ./hosts/thinkpad-x220)
+
+       inputs.home.nixosModules.home-manager
+       (import ./modules/home.nix)
+
+       inputs.nixpkgs.nixosModules.notDetected
+      ];
+      specialArgs = { inherit inputs; };
+    };
+    nixos = inputs.self.nixosConfigurations.nixos.config.system.build.toplevel;
+  };
+}
