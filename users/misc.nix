beefc47a4f90974068b64497a05340d0ab245b35
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-18 05:25:51 +0000

    Add gtk and xdg configuration

--- users/misc.nix
+++ users/misc.nix
@@ -1,12 +1,21 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }: {
 
-{
   home-manager.users.rafiyq = {    
     gtk = {
       enable = true;
-      font = { name = "Noto Sans 11"; package = pkgs.noto-fonts; };
-      iconTheme = { name = "Yaru"; package = pkgs.yaru-theme; };
-      theme = { name = "Yaru"; package = pkgs.yaru-theme; };
+      font = {
+        name = "Noto Sans 11";
+        package = pkgs.noto-fonts;
+      };
+      iconTheme = {
+        name = "Yaru";
+        package = pkgs.yaru-theme;
+      };
+      theme = {
+        name = "Yaru";
+        package = pkgs.yaru-theme;
+      };
+      #gtk2.extraConfig = { };
       gtk3.extraConfig = {
         gtk-xft-antialias = 1;
         gtk-xft-hinting = 1;
@@ -17,6 +26,7 @@
     xdg = {
       enable = true;
       userDirs.enable = true; 
+      mimeApps.enabel = true;
     };
   };
 }
