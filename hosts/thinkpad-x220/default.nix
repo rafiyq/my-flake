a7ed96bd5464917a44e61e2495dd466c2b2ba370
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-19 04:46:48 +0000

    Change configuration for thinkpad-x220

---
+++ hosts/thinkpad-x220/default.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./configuration.nix
+    ../../modules/uefi-zfs.nix
+    ../../modules/hardware.nix
+    ../../modules/nix-unstable.nix
+    ../../modules/users.nix
+    ../../modules/fonts.nix
+    ../../modules/xserver.nix
+  ];
+}
